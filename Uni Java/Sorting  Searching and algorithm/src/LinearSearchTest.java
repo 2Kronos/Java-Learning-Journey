e2a7f4b3d69ec9544b49d9605c90d22893419f2d
2Kronos <rayachrinovic@gmail.com>
2024-10-09 11:27:53 +0200

    modify

--- Uni Java/Sorting  Searching and algorithm/src/LinearSearchTest.java
+++ Uni Java/Sorting  Searching and algorithm/src/LinearSearchTest.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class LinearSearchTest {
     // perform a linear search on the data
-    public static int linearSearch(int[] data, int searchKey) {
-        // loop through array sequentially
-        for (int index = 0; index < data.length; index++) {
-            if (data[index] == searchKey)
-                return index; // return index of integer
-        }
+//    public static int linearSearch(int[] data, int searchKey) {
+//        // loop through array sequentially
+//        for (int index = 0; index < data.length; index++) {
+//            if (data[index] == searchKey)
+//                return index; // return index of integer
+//        }
 
-        return -1; // integer was not found
-    }
+      //  return -1; // integer was not found
+   // }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        
+z
         int[] dataArray = { 20, 50, 30, 10, 40 };
         System.out.println("Enter integer search key: ");
         
